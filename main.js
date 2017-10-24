512b9fc6c2eb5a4a1a02c7a618c26593619236c4
Little Big Owl <littlebigowl@outlook.com>
2017-10-24 16:15:08 +0200

    test changes 2

--- main.js
+++ main.js
@@ -57,12 +57,8 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
     logo.addEventListener("click", function () {
-        // var topTo = logo.offsetTop;
-        // var currentTop = getActualScrollTop();
-        // if (currentTop > topTo + 100 || currentTop < topTo - 100) {
-        //     window.scrollTo(0, 0);
-        // }
-
+        
+        window.scrollTo(0, 0)
         changeHeading();
     });
   
